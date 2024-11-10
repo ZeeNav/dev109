7be52313083f12419fecf98fa5173cef4bf1e132
ZeeNav <Navdeep.singh@bellevuecollege.edu>
2024-11-09 23:26:00 -0800

    Update Script.js

--- m08/Script.js
+++ m08/Script.js
@@ -1,1 +1,18 @@
+function addItem() {
+  var newItem = document.getElementById('newItemInput').value;
+
+  if (newItem.trim() !== "") {
+
+    var newEl = document.createElement('li');
+    newEl.className = 'hot';  
+
+    var newText = document.createTextNode(newItem);
+    newEl.appendChild(newText);
+
+    var position = document.getElementById('todo');
+    position.appendChild(newEl);
+
+    document.getElementById('newItemInput').value = '';
+  }
+}
 
