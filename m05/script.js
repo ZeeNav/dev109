0edb8baa5a8b687d060f9d20cdca9b158f1247d4
ZeeNav <Navdeep.singh@bellevuecollege.edu>
2025-06-21 13:15:58 -0700

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -1,13 +1,12 @@
-// Prompt the user for a number between 0 and 10
+
 let number = prompt('Enter a number between 0 and 10:');
 
-// Convert the input to a number and set to 0 if invalid
+
 number = Number(number);
 if (isNaN(number) || number < 0 || number > 10) {
   number = 0;
 }
 
-// Start building the HTML output for the multiplication table
 let msg = '<h2>Multiplication Table</h2>';
 
 // Use a loop to generate the table from 1 to 10
