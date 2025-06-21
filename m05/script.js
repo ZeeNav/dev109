41d5900a90c70d05485b587a6a95703c9fc6461d
ZeeNav <Navdeep.singh@bellevuecollege.edu>
2025-06-21 13:16:25 -0700

    Update script.js

--- m05/script.js
+++ m05/script.js
@@ -7,10 +7,8 @@ if (isNaN(number) || number < 0 || number > 10) {
 
 let msg = '<h2>Multiplication Table</h2>';
 
-// Use a loop to generate the table from 1 to 10
 for (let i = 1; i <= 10; i++) {
   msg += `<p>${i} x ${number} = ${i * number}</p>`;
 }
 
-// Insert the table into the page
 document.getElementById('output').innerHTML = msg;
