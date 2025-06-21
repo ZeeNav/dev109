1a4c86a7ebebd79c16d0c0ecaaa4e4be1e31d989
ZeeNav <Navdeep.singh@bellevuecollege.edu>
2025-06-21 15:31:51 -0700

    Update script.js

--- a5/script.js
+++ a5/script.js
@@ -1,23 +1,23 @@
 const images = [
   {
-      src: "https://ceoworld.biz/wp-content/uploads/2021/08/Bugs-Bunny.jpg",
-      description: "Bugs Bunny"
+    src: "https://ceoworld.biz/wp-content/uploads/2021/08/Bugs-Bunny.jpg",
+    description: "Bugs Bunny"
   },
   {
-      src: "https://thewittyblog.com/wp-content/uploads/2020/04/pooh-e1587660064143.jpg",
-      description: "Winnie the Pooh"
+    src: "https://thewittyblog.com/wp-content/uploads/2020/04/pooh-e1587660064143.jpg",
+    description: "Winnie the Pooh"
   },
   {
-      src: "https://occ-0-8407-116.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABR6vn8BtaC8r6FhDOfMCliOri5vm4p5bCnxzV61blyd_QJP0qr8VdYGGyoFxkLfwDc1nDVqf4ilKrmD1XquOAlXD29EyAM4UbC4i.jpg?r=54a",
-      description: "Ben 10"
+    src: "https://occ-0-8407-116.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABR6vn8BtaC8r6FhDOfMCliOri5vm4p5bCnxzV61blyd_QJP0qr8VdYGGyoFxkLfwDc1nDVqf4ilKrmD1XquOAlXD29EyAM4UbC4i.jpg?r=54a",
+    description: "Ben 10"
   },
   {
-      src: "https://i.redd.it/wdfo3bbxdvva1.jpg",
-      description: "Scooby-Doo"
+    src: "https://i.redd.it/wdfo3bbxdvva1.jpg",
+    description: "Scooby-Doo"
   },
   {
-      src: "https://m.media-amazon.com/images/S/pv-target-images/4f995dde6b43fbe45bab9336476a66df17cc83270f06bc5e73492d21edcf7bef.jpg",
-      description: "Tom and Jerry"
+    src: "https://m.media-amazon.com/images/S/pv-target-images/4f995dde6b43fbe45bab9336476a66df17cc83270f06bc5e73492d21edcf7bef.jpg",
+    description: "Tom and Jerry"
   }
 ];
 
@@ -28,11 +28,13 @@ const imageDescription = document.getElementById('image-description');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 
-const updateImage = () => {
+function updateImage() {
   imageDisplay.src = images[currentIndex].src;
+  imageDisplay.alt = images[currentIndex].description;
   imageDescription.textContent = images[currentIndex].description;
-};
+}
 
+// Button event listeners
 prevBtn.addEventListener('click', () => {
   currentIndex = (currentIndex - 1 + images.length) % images.length;
   updateImage();
@@ -43,17 +45,20 @@ nextBtn.addEventListener('click', () => {
   updateImage();
 });
 
+// Auto slideshow
 let autoSlide = setInterval(() => {
   currentIndex = (currentIndex + 1) % images.length;
   updateImage();
 }, 5000);
 
+// Pause on mouse over, resume on mouse out
 imageDisplay.addEventListener('mouseover', () => clearInterval(autoSlide));
 imageDisplay.addEventListener('mouseout', () => {
   autoSlide = setInterval(() => {
-      currentIndex = (currentIndex + 1) % images.length;
-      updateImage();
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
   }, 5000);
-});  
+});
 
+// Initial display
 updateImage();
